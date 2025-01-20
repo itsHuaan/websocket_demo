2be668b3b3e5e4f0d53eb1c4dd639bcc35a172ea
itsHuaan <doquanghuan1909@gmail.com>
2025-01-20 11:12:29 +0700

    refactor(exception): delete unused code

--- src/main/java/com/example/websocket_demo/configuration/exception/Forbidden.java
+++ src/main/java/com/example/websocket_demo/configuration/exception/Forbidden.java
@@ -17,13 +17,9 @@ import java.util.Map;
 @Component
 public class Forbidden implements AccessDeniedHandler {
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response,
-            AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        // String checkRequest = request.getRequestURI().split("/")[1];
-        // if(checkRequest.equals("admin")){ //For exception admin thymleaf
-        // response.sendRedirect("/403");
-        // return;
-        // }
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         final Map<String, Object> body = new HashMap<>();
